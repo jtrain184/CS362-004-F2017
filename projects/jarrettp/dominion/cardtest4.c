7b2ee23d437a2d2d116d833d8ff7869b5aea167c
Bman <Bman>
2017-10-23 02:01:42 -0400

    adding smihty test case

--- projects/jarrettp/dominion/cardtest4.c
+++ projects/jarrettp/dominion/cardtest4.c
@@ -36,7 +36,7 @@ int main() {
     int thisPlayer = 0;
 	struct gameState G, testG;
 	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
-			sea_hag, tribute, smithy, council_room};
+			sea_hag, tribute, steward, council_room};
 
 	// initialize a game state and player cards
 	initializeGame(numPlayers, k, seed, &G);
@@ -51,7 +51,7 @@ int main() {
 	choice1 = 1;
 	cardEffect(smithy, choice1, choice2, choice3, &testG, handpos, &bonus);
 
-	newCards = 2;
+	newCards = 3;
 	xtraCoins = 0;
 	printf("hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer] + newCards - discarded);
 	printf("deck count = %d, expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - newCards + shuffledCards);
