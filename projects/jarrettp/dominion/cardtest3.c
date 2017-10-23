e754904f9ee47ea8d9c06aa27cb3d24bec43fe8f
Bman <Bman>
2017-10-23 02:33:02 -0400

    Great hall and village tests

--- projects/jarrettp/dominion/cardtest3.c
+++ projects/jarrettp/dominion/cardtest3.c
@@ -1,14 +1,11 @@
 /*
- * cardtest1.c
+ * cardtest3.c - great hall
  *
  
  */
 
 /*
- * Include the following lines in your makefile:
- *
- * cardtest4: cardtest4.c dominion.o rngs.o
- *      gcc -o cardtest1 -g  cardtest4.c dominion.o rngs.o $(CFLAGS)
+
  */
 
 
@@ -20,7 +17,7 @@
 #include "rngs.h"
 #include <stdlib.h>
 
-#define TESTCARD "adventurer"
+#define TESTCARD "great_hall"
 
 int main() {
     int newCards = 0;
@@ -44,13 +41,14 @@ int main() {
 	printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
 
 	// ----------- TEST 1: choice1 = 1 = +3 cards --------------
-	printf("TEST 1: Adventurer card effects\n");
+	printf("TEST 1: Great Hall card effects\n");
 
 	// copy the game state to a test case
 	memcpy(&testG, &G, sizeof(struct gameState));
-	cardEffect(adventurer, choice1, choice2, choice3, &testG, handpos, &bonus);
+	cardEffect(great_hall, choice1, choice2, choice3, &testG, handpos, &bonus);
 
 	xtraCoins = 0;
+	newCards = 1;
 	printf("hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer] + newCards - discarded);
 	printf("deck count = %d, expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - newCards + shuffledCards);
 	printf("coins = %d, expected = %d\n", testG.coins, G.coins + xtraCoins);
