b5ccfbdb02c66de378fa1f7f10e2960de23baa82
Bman <Bman>
2017-11-06 01:15:57 -0500

    refactor adventurer

--- projects/jarrettp/dominion/randomtestadventurer.c
+++ projects/jarrettp/dominion/randomtestadventurer.c
@@ -1,55 +1,68 @@
 #include "dominion.h"
 #include "dominion_helpers.h"
-#include "rngs.h"
+#include <string.h>
 #include <stdio.h>
-#include <math.h>
+#include <assert.h>
 #include <stdlib.h>
+#include <time.h>
+#include "rngs.h"
 
-#define MAX_TESTS 12
-#define MAX_TEST_DECK 30
-#define MAX_TEST_HAND 15
-
-//This randomly tests Adventurer
-
-int main() {
-
-	  int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, 
-	       sea_hag, tribute, smithy};
-
-	  int i, j, n; 
-          int players = 0, player = 0, seed = 0;
-          int handPos = 0;
-          int bonus = 0;
-	  struct gameState state;
-
-	  printf("Running Random Adventurer Test\n");
+#define DEBUG 0
 
 
-	  for (i = 0; i < MAX_TESTS; i++) {
-
-	   players = rand() % 4;
-
-	   seed = rand();		//pick random seed
-		
-	   initializeGame(players, k, seed, &state);	//initialize Gamestate 
-
-	   //Initiate valid state variables
-		  state.deckCount[player] = rand() % MAX_TEST_DECK; //Random deck size
-		  state.discardCount[player] = rand() % MAX_TEST_DECK;
-		  state.handCount[player] = rand() % MAX_TEST_HAND;
-
-
-		  // Chance to make deck empty for coverage
-		  if (seed % 3 == 0) {
-
-			state.deckCount[player] = 0;
-		  }
-		  cardEffect(adventurer, 1, 1, 1, &state,handPos,&bonus);	
-              	
-	  }
-	  
-
-	  printf("Tests Complete\n");
-
-	  return 0;
+int random_number(int min_num, int max_num){
+    int result=0,low_num=0,hi_num=0;
+    if(min_num<max_num)
+    {
+        low_num=min_num;
+        hi_num=max_num+1; // this is done to include max_num in output.
+    }else{
+        low_num=max_num+1;// this is done to include max_num in output.
+        hi_num=min_num;
+    }
+    srand(time(NULL));
+    result = (rand()%(hi_num-low_num))+low_num;
+    return result;
 }
+
+int main(int argc, char const *argv[]){
+	
+	struct gameState game;
+
+	int i, j;
+	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+	int myCard;
+	int currentplayer = 0;
+	int cardsInHand = 0;	
+
+	//adventurer specific variables
+	int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+
+
+	for (i = 0; i < random_number(0, 100); i ++){ //Loops a random number of times <100
+		initializeGame(random_number(2,4), k, random_number(1, 9), &game); //initializes a game with a random seed
+															//between 1-9
+
+		for (j = 0; j < 10000; j++){ // run 10000 times with random variables in this game state
+			currentplayer = game.whoseTurn;
+			cardsInHand = game.handCount[currentplayer];  //before smithy effect
+			cardEffect(adventurer, choice1, choice2, choice3, &game, handpos, &bonus);
+			
+
+			if(game.handCount[currentplayer]  != cardsInHand + 2)  //Did we draw two treasure cards?
+				printf("Error: adventurer did not return 2 treasure cards. Random Game[%d] iteration[%d]\n", i, j);
+			    
+			
+			game.outpostPlayed = 0;
+  			game.phase = 0;
+			game.numActions = 1;
+			game.numBuys = 1;
+			game.playedCardCount = 0;
+			game.whoseTurn = random_number(2, game.numPlayers);
+			game.handCount[game.whoseTurn] = 0;
+		}
+	}
+
+	return 0;
+}
+
