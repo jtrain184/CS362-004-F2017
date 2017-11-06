0e68e58de7d759671ade0ad17a0588228563a89f
Bman <Bman>
2017-11-05 23:22:09 -0500

    update random adventurer tests

--- projects/jarrettp/dominion/randomtestadventurer.c
+++ projects/jarrettp/dominion/randomtestadventurer.c
@@ -7,6 +7,8 @@
 #include <assert.h>
 
 #define MAX_TESTS 12
+#define MAX_TEST_DECK 50
+#define MAX_TEST_HAND 15
 
 //This randomly tests Adventurer
 
@@ -40,9 +42,9 @@ int main() {
 	   initializeGame(players, k, seed, &state);	//initialize Gamestate 
 
 	   //Initiate valid state variables
-		  state.deckCount[player] = rand() % MAX_DECK; //Pick random deck size out of MAX DECK size
-		  state.discardCount[player] = rand() % MAX_DECK;
-		  state.handCount[player] = rand() % MAX_HAND;
+		  state.deckCount[player] = rand() % MAX_TEST_DECK; //Random deck size
+		  state.discardCount[player] = rand() % MAX_TEST_DECK;
+		  state.handCount[player] = rand() % MAX_TEST_HAND;
 
 
 		  //Copy state variables
@@ -57,57 +59,6 @@ int main() {
 		  cardEffect(adventurer, 1, 1, 1, &state);		//Run adventurer card
 	  }
 	  
-	   for (i = 0; i < MAX_TESTS; i++) {
-
-  		  players = rand() % 4;
-		  seed = rand();		//pick random seed
-		
-		  initializeGame(players, k, seed, &stat);	//initialize Gamestate
-
-		  //Initiate valid state variables
-		  stat.deckCount[player] = rand() % MAX_DECK; //Pick random deck size out of MAX DECK size
-		  stat.discardCount[player] = rand() % MAX_DECK;
-		  stat.handCount[player] = rand() % MAX_HAND;
-
-
-		  //Copy state variables
-		  handCount = stat.handCount[player];
-		  deckCount = stat.deckCount[player];
-
-		  //1 in 3 chance of making empty deck for coverage
-		  if (seed % 3 == 0) {
-
-			stat.deckCount[player] = 0;
-		  }
-
-		  cardEffect(adventurer, 1, 1, 1, &stat);		//Run adventurer card
-	  }
-
-	   for (i = 0; i < MAX_TESTS; i++) {
-
-  		  players = rand() % 4;
-		  seed = rand();		//pick random seed
-		
-		  initializeGame(players, k, seed, &sta);	//initialize Gamestate
-
-		  //Initiate valid state variables
-		  sta.deckCount[player] = rand() % MAX_DECK; //Pick random deck size out of MAX DECK size
-		  sta.discardCount[player] = rand() % MAX_DECK;
-		  sta.handCount[player] = rand() % MAX_HAND;
-
-
-		  //Copy state variables
-		  handCount = sta.handCount[player];
-		  deckCount = sta.deckCount[player];
-
-		  //1 in 3 chance of making empty deck for coverage
-		  if (seed % 3 == 0) {
-
-			sta.deckCount[player] = 0;
-		  }
-		  cardEffect(adventurer, 1, 1, 1, &sta);		//Run adventurer card
-
-	   }
 
 	  printf("Tests Complete\n");
 
